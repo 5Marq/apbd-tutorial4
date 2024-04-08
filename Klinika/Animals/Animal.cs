458efb6214e928bf8e841dd5115e9ccd99cba365
5Marq <epgawin@yahoo.pl>
2024-04-08 13:11:55 +0200

    first commit

--- Klinika/Animals/Animal.cs
+++ Klinika/Animals/Animal.cs
@@ -2,5 +2,9 @@ namespace Klinika.Animals;
 
 public class Animal
 {
-    
+    public int id { get; set; }
+    public String name { get; set; }
+    public String type { get; set; }
+    public double weight { get; set; }
+    public String furColour { get; set; }
 }
--- Klinika/Animals/Configuartion.cs
+++ Klinika/Animals/Configuartion.cs
@@ -4,9 +4,10 @@ public static class Configuartion
 {
     public static void RegisterEndpointsForAnimals(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/api/animals", () =>
+        endpoints.MapGet("/api/v1/animals", () =>
         {
-            
-        }  
+            return Results.Ok("All animals");
+        });
+        } 
+        
     }
-}
